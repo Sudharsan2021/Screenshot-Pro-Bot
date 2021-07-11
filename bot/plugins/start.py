cdcc064437dbd44b401ac0c36789b787731958b9
Sudharsan2021 <83271133+Sudharsan2021@users.noreply.github.com>
2021-07-11 03:43:53 +0000

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -20,12 +20,12 @@ async def start(c, m):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('📌  Support Group', url='https://t.me/InFoTelGroup'),
-                    InlineKeyboardButton('🔖  Projects Channel', url='https://t.me/TGBotsZ')
+                    InlineKeyboardButton('📌  Support Group', url=''),
+                    InlineKeyboardButton('🔖  Projects Channel', url='')
                 ],
                 [
-                    InlineKeyboardButton('💡  Source Code', url='https://github.com/TGExplore/Screenshot-Bot'),
-                    InlineKeyboardButton('👨  Master', url='https://t.me/odbots')
+                    InlineKeyboardButton('💡  Source Code', url=''),
+                    InlineKeyboardButton('👨  Master', url='')
                 ]
             ]
         )
